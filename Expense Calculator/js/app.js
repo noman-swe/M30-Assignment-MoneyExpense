a70d4e2ab1a2530e4b2169ec6daea27be9309805
noman-swe <noman.swe130@gmail.com>
2022-08-17 02:40:37 +0600

    Expense Project Done

--- Expense Calculator/js/app.js
+++ Expense Calculator/js/app.js
@@ -1,113 +1,117 @@
-// checking input is a number or not 
-function checkInputIsInteger(errorIdPart){
-    const inputField = document.getElementById(errorIdPart+'-field');
-    const inputValue = inputField.value;
-    
-    const numberError = document.getElementById('number-input-error');
-    
-    if(isNaN(inputValue)){
-        // console.log('Please input a Number');
-        numberError.style.textAlign = 'center';
-        numberError.style.color = 'red';
-        numberError.style.paddingTop = '15px';
-        numberError.innerText = 'Please input a Number.';
-    }
-    else if(inputValue < 0){
-        // not working properly
-        console.log('0 theke soto');
-        numberError.innerText = 'Please input a Number.';
-    }
-    else{
-        numberError.style.display = 'none';
-        inputField.value = '';
-        return inputValue;
-    }
+function getInputValue(inputIdPart) {
+    const inputField = document.getElementById(inputIdPart + '-field');
+    const inputValueTxt = inputField.value;
+    const inputValue = parseFloat(inputValueTxt);
+
     inputField.value = '';
+    return inputValue;
+}
+
+// 
+function nullEntryMsg(errorIdPart) {
+    const error = document.getElementById(errorIdPart + '-error');
+
+    if (errorIdPart == 'income') {
+        error.innerText = ('Income is required with integer numbers!');
+    }
+    if (errorIdPart == 'food') {
+        error.innerText = ('Food is required with integer numbers!');
+    }
+
+    if (errorIdPart == 'rent') {
+        error.innerText = ('Rent required with integer numbers!');
+    }
+
+    if (errorIdPart == 'clothes') {
+        error.innerText = ('Clothes is required with integer numbers!');
+    }
+
+}
+
+//string error
+function stringError(errorIdPart) {
+    const errorId = document.getElementById(errorIdPart + '-error');
+    errorId.innerText = 'Integer is required!';
 }
 
 
-// calculate function
-function getcalculateValue(errorIdPart){
-    const incomeField = document.getElementById(errorIdPart+'-field');
-    // const incomeInput = incomeField.value;
-    const incomeInput = checkInputIsInteger(errorIdPart+'field');
-    const incomeInputValue = parseFloat(incomeInput); //Input Value
-    return (incomeInputValue);
+// 
+
+function checkInputValue(inputIdPart) {
+    // got value
+    const inputValue = getInputValue(inputIdPart);
+    const nullEntry = nullEntryMsg(inputIdPart);
+    const errorId = document.getElementById(inputIdPart + '-error');
+    //check value
+    if (inputValue == null || isNaN(inputValue)) {
+        return (nullEntry);
+    }
+    else {
+        errorId.style.display = 'none';
+        return (inputValue);
+    }
 }
 
-//errors for null entry
-function nullEntryMsg(errorIdPart){
-    const error = document.getElementById(errorIdPart+'-error');
-    if(errorIdPart == 'income'){
-        error.innerText =('Income is empty!');
-    }
-    if(errorIdPart == 'food'){
-        error.innerText =('Food is empty!');
-    }
-    
-    if(errorIdPart == 'rent'){
-        error.innerText =('Rent is empty!');
-    }
-    
-    if(errorIdPart == 'clothes'){
-        error.innerText =('Clothes is empty!');
-    }
-    
+
+function expenses() {
+    // food
+    const foodInputValue = checkInputValue('food');
+    // income
+    const rentInputValue = checkInputValue('rent');
+    // income
+    const clothesInputValue = checkInputValue('clothes');
+    // total 
+    const totalExpense = foodInputValue + rentInputValue + clothesInputValue;
+
+    return (totalExpense);
+
+}
+
+// save amount
+function getSaveAmount(inputIdPart) {
+    // console.log(existingBalance);
+    const saveField = document.getElementById(inputIdPart + '-field');
+    const saveInputValue = getInputValue(inputIdPart);
+    // console.log(saveInputValue);
+
+    const existingBalanceField = document.getElementById('existing-balance');
+    const existingBalanceTxt = existingBalanceField.innerText;
+    const existingBalanceValue = parseFloat(existingBalanceTxt);
+    const saveAmount = (existingBalanceValue * saveInputValue) / 100;
+    return saveAmount;
 }
 
 
 
 // calculateButton
 document.getElementById('cal-btn').addEventListener('click', () => {
-    // error
-    const incomeError = document.getElementById('income-error');
-    // Income field
-    const incomeInputValue = getcalculateValue('income-field');
-    if(!isNaN(incomeInputValue)){
-        incomeError.style.display = 'none';
-        return (incomeInputValue);
-    }
-    else{
-        nullEntryMsg('income');
-        
-    }
-    // food field
-    const foodInputValue = getcalculateValue('food-field');
-    // console.log(foodInputValue);
-    const foodError = document.getElementById('food-error');
-    if(!isNaN(foodInputValue)){
-        foodError.style.display = 'none';
-        return (foodInputValue);
-    }
-    else{
-        nullEntryMsg('food');
-    }
-    
-    // rent
-    const rentInputValue = getcalculateValue('rent-field');
-    // console.log(rentInputValue);
-    const rentError = document.getElementById('rent-error');
-    if(!isNaN(rentInputValue)){
-        rentError.style.display = 'none';
-        return (rentInputValue);
-    }
-    else{
-        nullEntryMsg('rent');
-    }
-    
-    // clothes
-    const clothesInputValue = getcalculateValue('clothes-field');
-    const clothrsError = document.getElementById('clothes-error');
-    if(!isNaN(clothesInputValue)){
-        clothrsError.style.display = 'none';
-        return (clothesInputValue);
-    }
-    else{
-        nullEntryMsg('clothes')
-    }
-    // console.log(clothesInputValue);
+    // income
+    const incomeInputValue = checkInputValue('income');
 
-    const totalExpense = foodInputValue + rentInputValue + clothesInputValue;
-    // console.log(totalExpense);
+    const totalExpense = expenses();
+    //  set total expense to total Expense section
+    document.getElementById('total-expense').innerText = totalExpense;
 
+    //   calculate Existing Balance 
+    const existingBalanceField = document.getElementById('existing-balance');
+    const existingBalance = incomeInputValue - totalExpense;
+    // const existingBalance = getExistingBalance();
+    existingBalanceField.innerText = existingBalance;
+});
+
+// save section
+document.getElementById('saving-btn').addEventListener('click', () => {
+    const saveAmount = getSaveAmount('saving');
+    // console.log(saveAmount);
+    document.getElementById('saving-amount').innerText = saveAmount;
+
+    // remaining balance
+    const remainingField = document.getElementById('remaining-amount');
+    const existingBalanceTxt = document.getElementById('existing-balance').innerText;
+    const existingBalanceValue = parseFloat(existingBalanceTxt);
+
+    // calculate
+    const remainingBalance = existingBalanceValue - saveAmount;
+    remainingField.innerText = remainingBalance;
+   
 });
--- Expense Calculator/js/script.js
+++ Expense Calculator/js/script.js
@@ -12,9 +12,6 @@ function nullEntryMsg(errorIdPart) {
     const error = document.getElementById(errorIdPart + '-error');
 
     if (errorIdPart == 'income') {
-        // 
-
-        // 
         error.innerText = ('Income is required with integer numbers!');
     }
     if (errorIdPart == 'food') {
@@ -46,15 +43,80 @@ function checkInputValue(inputIdPart) {
     const nullEntry = nullEntryMsg(inputIdPart);
     const errorId = document.getElementById(inputIdPart + '-error');
     //check value
-     if (inputValue == null || isNaN(inputValue)) {
+    if (inputValue == null || isNaN(inputValue)) {
         console.log(nullEntry);
+    }
+    else {
+        errorId.style.display = 'none';
+        return (inputValue);
     }
 }
 
 
+function expenses() {
+
+    // food
+    const foodInputValue = checkInputValue('food');
+    // income
+    const rentInputValue = checkInputValue('rent');
+    // income
+    const clothesInputValue = checkInputValue('clothes');
+
+    // total 
+    const totalExpense = foodInputValue + rentInputValue + clothesInputValue;
+
+    return (totalExpense);
+
+}
+
+
+function getSaveAmount(inputIdPart) {
+    // console.log(existingBalance);
+    const saveField = document.getElementById(inputIdPart + '-field');
+    const saveInputValue = getInputValue(inputIdPart);
+    console.log(saveInputValue);
+
+    const existingBalanceField = document.getElementById('existing-balance');
+    const existingBalanceTxt = existingBalanceField.innerText;
+    const existingBalanceValue = parseFloat(existingBalanceTxt);
+    const saveAmount = (existingBalanceValue * saveInputValue) / 100;
+    return saveAmount;
+}
+
+
+function getExistingBalance() {
+    const incomeInputValue = checkInputValue('income');
+    const totalExpense = expenses();
+    //   calculate Existing Balance 
+    const existingBalance = incomeInputValue - totalExpense;
+    return existingBalance;
+}
 
 // calculateButton
 document.getElementById('cal-btn').addEventListener('click', () => {
-    const filteredInputValue = checkInputValue('income');
+    // income
+    const incomeInputValue = checkInputValue('income');
 
+    const totalExpense = expenses();
+    //  set total expense to total Expense section
+    document.getElementById('total-expense').innerText = totalExpense;
+
+    //   calculate Existing Balance 
+    const existingBalanceField = document.getElementById('existing-balance');
+    const existingBalance = incomeInputValue - totalExpense;
+    existingBalanceField.innerText = existingBalance;
+
+});
+
+// save section
+document.getElementById('saving-btn').addEventListener('click', () => {
+    const saveAmount = getSaveAmount('saving');
+    console.log(saveAmount);
+    document.getElementById('saving-amount').innerText = saveAmount;
+
+    // remaining balance
+    const existingBalance = getExistingBalance()
+    const remainingBalanceField = document.getElementById('ramaining-amount');
+    const remainingBalance = existingBalance - saveAmount;
+    remainingBalanceField.innerText = remainingBalance;
 });
